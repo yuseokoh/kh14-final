50a9669ba06ab8748c0a4fe486a2269083ad91f0
kjh0908 <kimmiyounl98@naver.com>
2024-10-18 14:50:33 +0900

    충돌해결

--- final/src/main/java/com/game/dao/GameDao.java
+++ final/src/main/java/com/game/dao/GameDao.java
@@ -10,50 +10,43 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.game.dto.GameDto;
-import com.game.mapper.GameMapper;
 
 @Repository
 public class GameDao {
 
-	@Autowired
-	private SqlSession sqlSession;
-	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	
-	@Autowired
-	private GameMapper gameMapper;
+    @Autowired
+    private SqlSession sqlSession;
 
-	public List<GameDto> selectList() {
-		return sqlSession.selectList("game.list");
-	}
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	//mybatis에서 사용하는 검색기능
-	public List<GameDto> selectList(String column, String keyword) {
-		Map<String, Object> params = new HashMap<>();
-		params.put("column", column);
-		params.put("keyword", keyword);
-		return sqlSession.selectList("emp.search", params);
-	}
+    public List<GameDto> selectList() {
+        return sqlSession.selectList("game.list");
+    }
 
-	public void insert(GameDto gameDto) {
-		sqlSession.insert("game.add", gameDto);		
-	}
+    //mybatis에서 사용하는 검색기능
+    public List<GameDto> selectList(String column, String keyword) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("column", column);
+        params.put("keyword", keyword);
+        return sqlSession.selectList("emp.search", params);
+    }
 
-	public boolean update(GameDto gameDto) {
-		int result = sqlSession.update("game.fix", gameDto);
-		return result > 0;
-	}
+    public void insert(GameDto gameDto) {
+        sqlSession.insert("game.add", gameDto);
+    }
 
-	public boolean delete(int gameNo) {
-		return sqlSession.delete("game.del", gameNo) > 0;
-	}
+    public boolean update(GameDto gameDto) {
+        int result = sqlSession.update("game.fix", gameDto);
+        return result > 0;
+    }
 
-	public GameDto selectOne(int gameNo) {
-		String sql = "select * from book where game_no = ?";
-		Object[] data = {gameNo};
-		List<GameDto> list = jdbcTemplate.query(sql, gameMapper, data);
-		return list.isEmpty() ? null : list.get(0);
-	}
-	
+    public boolean delete(int gameNo) {
+        return sqlSession.delete("game.del", gameNo) > 0;
+    }
+
+    public GameDto selectOne(int gameNo) {
+        return sqlSession.selectOne("game.detail", gameNo);
+    }
+
 }
